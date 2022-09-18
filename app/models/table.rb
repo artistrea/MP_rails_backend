ed677db709e53fad525f1b85a662e0b3d753fa6a
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-18 12:59:11 -0300

    linter happy again

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,5 @@
 class Table < ApplicationRecord
-  has_many :clients, class_name: "client", foreign_key: "client_id"
+  has_many :clients, class_name: 'client', foreign_key: 'client_id',
+                     dependent: :restrict_with_error, inverse_of: :table
   validates :number, uniqueness: true
 end
