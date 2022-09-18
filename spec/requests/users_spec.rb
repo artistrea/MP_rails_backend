fb50bed6cfbbebb0ef1e80a896924c6d7ecec167
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-18 18:30:20 -0300

    users create tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,42 @@
 RSpec.describe 'Users', type: :request do
   describe 'POST /create' do
     let(:admin) { create(:user, user_type: 2) }
-    let(:cook) { create(:user, user_type: 2) }
+    let(:cook) { create(:user, user_type: 0) }
+
     let(:admin_headers) { { 'X-User-Token': admin.authentication_token, 'X-User-Email': admin.email } }
     let(:cook_headers) { { 'X-User-Token': cook.authentication_token, 'X-User-Email': cook.email } }
 
-    it 'returns http success' do
+    valid_user_params = { email: 'teste@teste.com', password: '123456', user_type: 1 }
+    invalid_user_params = { email: 'teste@teste.com', password: '12345', user_type: 1 }
+
+    it 'returns http success if admin requested with valid params' do
       post '/users/create',
            params: {
-             user: { email: 'teste@teste.com', password: '123456', user_type: 1 }
+             user: valid_user_params
            },
            headers: admin_headers
 
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns unprocessable_entity if admin requested with invalid params' do
+      post '/users/create',
+           params: {
+             user: invalid_user_params
+           },
+           headers: admin_headers
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'returns unauthorized if cook requested' do
+      post '/users/create',
+           params: {
+             user: valid_user_params
+           },
+           headers: cook_headers
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 
