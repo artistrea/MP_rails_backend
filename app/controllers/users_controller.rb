a177086e16670cd2c9f3d38fffe0d9c75abf8ca4
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-18 18:30:44 -0300

    user create tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,19 @@ class UsersController < ApplicationController
   acts_as_token_authentication_handler_for User, except: [:login]
 
   def create
-    render json: { message: 'Não autorizado' }, status: :unauthorized if current_user.user_type < 2
+    if current_user.user_type < 2
+      render json: { message: 'Não autorizado' }, status: :unauthorized
+      return
+    end
     user = User.create!(user_params)
     render json: user, status: :ok
   rescue StandardError => e
     render json: { message: e }, status: :unprocessable_entity
   end
 
-  # def index; end
+  def index
+  
+  end
 
   # def login; end
 
