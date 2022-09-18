dcf6295b1da421a2dac61bc110dd0ec3afe246cb
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-18 12:57:12 -0300

    Revert "linter"
    This reverts commit baa4bf0223ce23083ba9ce9d7927b043e5121e9a.

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,5 +1,4 @@
 class Table < ApplicationRecord
-  has_many :clients, class_name: 'client', foreign_key: 'client_id',
-                     dependent: :restrict_with_error, inverse_of: :table
+  has_many :clients, class_name: "client", foreign_key: "client_id"
   validates :number, uniqueness: true
 end
