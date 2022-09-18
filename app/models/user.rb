d84c66a014825ce819c95390115d679b4a8f5d79
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-18 13:32:45 -0300

    relation fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :orders
   validates :user_type, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 2 }
 
   devise :database_authenticatable, :registerable,
