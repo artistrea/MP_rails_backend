a02774dad6f750bd6bad0d18251a5e13b920a784
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-09-18 13:40:34 -0300

    test passing

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :order do
-    user { nil }
-    client { nil }
-    products { [] }
+    sequence(:user) { |n| create(:user, email: "admi@mail#{n}teste.com") }
+    sequence(:client) { |n| create(:client) }
+    sequence(:products) { |n| [create(:product, name: "Produto num #{n}")] }
     status { 1 }
   end
 end
