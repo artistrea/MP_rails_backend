3c6c6ec4839ddc6bbdef4a5787ee1b88bb03f60d
tlamen <bernardolobo3@gmail.com>
2022-09-19 16:23:13 -0300

    :bug: devise skip all

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
   
   # For staff to login, admin to edit staff
   scope "users" do
